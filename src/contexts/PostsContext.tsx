37ba6887cce4673ff2b5914bc18a63598d358ebe
jillusc <jillus@hotmail.com>
2025-09-28 10:24:55 +0200

    Create context + provider for sharing posts state globally

---
+++ src/contexts/PostsContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+import type { Post } from "../types";
+
+// type for the context value (posts + setter)
+type PostsContextType = {
+  posts: Post[]; // array of posts
+  setPosts: React.Dispatch<React.SetStateAction<Post[]>>;
+};
+
+// context to hold the posts + setter (uses type as above):
+export const PostsContext = createContext<PostsContextType | null>(null);
+
+// Provider component creates/defines state:
+export const PostsProvider = ({ children }: { children: ReactNode }) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  return (
+    <PostsContext.Provider value={{ posts, setPosts }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
+
+// hook to get the value of state in line 15:
+export const usePosts = () => {
+  const context = useContext(PostsContext);
+  if (!context) throw new Error("usePosts must be used within a PostsProvider");
+  return context.posts;
+};
+
+// hook to get the state setter in line 15:
+export const useSetPosts = () => {
+  const context = useContext(PostsContext);
+  if (!context)
+    throw new Error("useSetPosts must be used within a PostsProvider");
+  return context.setPosts;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
+import { PostsProvider } from "./contexts/PostsContext.tsx";
 import { system } from "./theme";
 import "./index.css";
 import App from "./App.tsx";
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <ChakraProvider value={system}>
         <CurrentUserProvider>
-          <App />
+          <PostsProvider>
+            <App />
+          </PostsProvider>
         </CurrentUserProvider>
       </ChakraProvider>
     </BrowserRouter>
